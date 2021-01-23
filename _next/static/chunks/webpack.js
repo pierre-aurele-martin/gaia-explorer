4fabe21c67293641e09613d131f38e56dc91a008
Pierre Aurèle <pierre.aurele.martin@gmail.com>
2021-01-23 21:33:53 +0100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6bfe1e1b611fd4f205f8";
+/******/ 	var hotCurrentHash = "9bc3d5fa4ef17a5cdf0a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
