8e7f5157314f3296c9c51fea2071547297eef12e
Pierre Aurèle <pierre.aurele.martin@gmail.com>
2021-01-24 11:13:08 +0100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9f89a5be5f132e71544a";
+/******/ 	var hotCurrentHash = "45ef14127ceca48e6060";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
